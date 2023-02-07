3b09f065f64bab4535cfc9d76b55461bb538d296
Shubham Suresh Kanozia <115910167+Shubhamk0154@users.noreply.github.com>
2023-02-07 12:12:38 +0530

    Add files via upload

---
+++ rest/src/main/java/com/app/pojos/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.app.pojos;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import lombok.Getter;
+
+@MappedSuperclass // mandatory cls level anno to tell hibernate that following is a common base
+	@Getter				// class to extend entities , no table generation
+public class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
